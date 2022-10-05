c52d4ee1f3e51c771d403fc599212ca15f7cb843
Inzamam Ahmad <76010008+inzamamahmad3@users.noreply.github.com>
2022-10-04 18:34:12 -0700

    Responsiveness of About page
    About page the website is changed a bit

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ import Picture from "../public/inzama-ahmad.jpg";
 const about = () => {
   return (
     <div>
-      <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 ">
+      <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 sm:p-1 md-p-4 lg:p-6 ">
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-3xl font-sans m-6 font-bold">
             Hello EveryOne, I&apos;m Inzamam Ahmad
@@ -20,7 +20,7 @@ const about = () => {
               alt="inzamam-ahmad"
             />
           </div>
-          <div className="m-10 p-10">
+          <div className="m-2 mb-3 sm:m-3 md:m-6 lg:m-10 p-2 sm:p-3 md:p-6 lg:p-10">
             <p className="m-auto text-xl ">
               I&apos;m a full stack software engineer. Currently Learning
               Blockchain Developement.
